9458020ed2363187ab34c432e017ee9ed699bc42
NMT03 <nmtgamer03@gmail.com>
2024-02-29 12:14:35 +0700

    29/2:12h

--- src/routes/trangchu.js
+++ src/routes/trangchu.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const trangchucontroller = require("../app/controllers/trangchucontroller");
 const logincontroller = require("../app/controllers/logincontroller");
 
-router.get("/", logincontroller.check_per, trangchucontroller.index);
+router.get("/", trangchucontroller.index);
 
 module.exports = router;
